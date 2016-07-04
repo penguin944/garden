2795a1a94b47e45206a824b2efcf4fc63ddbb849
Mike Pleimann <mike@pleimann.com>
2016-07-04 15:48:00 -0500

    first stab at integrating angular2 material

--- public/app/app.component.js
+++ public/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '@angular/router', "@angular/common", './feeds/feed-list.component', './feeds/feed-detail.component', './alerts.service'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/router', "@angular/common", '@angular2-material/button', '@angular2-material/sidenav', '@angular2-material/icon', './feeds/feed-list.component', './feeds/feed-detail.component', './alerts.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['@angular/core', '@angular/router', "@angular/common", './feeds
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1, common_1, feed_list_component_1, feed_detail_component_1, alerts_service_1;
+    var core_1, router_1, common_1, button_1, sidenav_1, icon_1, feed_list_component_1, feed_detail_component_1, alerts_service_1;
     var AppComponent;
     return {
         setters:[
@@ -22,6 +22,15 @@ System.register(['@angular/core', '@angular/router', "@angular/common", './feeds
             },
             function (common_1_1) {
                 common_1 = common_1_1;
+            },
+            function (button_1_1) {
+                button_1 = button_1_1;
+            },
+            function (sidenav_1_1) {
+                sidenav_1 = sidenav_1_1;
+            },
+            function (icon_1_1) {
+                icon_1 = icon_1_1;
             },
             function (feed_list_component_1_1) {
                 feed_list_component_1 = feed_list_component_1_1;
@@ -37,14 +46,24 @@ System.register(['@angular/core', '@angular/router', "@angular/common", './feeds
                 function AppComponent(alertsService) {
                     var _this = this;
                     this.alerts = [];
+                    this.title = "Pleimann - Garden Dashboard";
+                    this.views = [
+                        {
+                            name: 'feeds',
+                            description: 'Current weather in St. Louis',
+                            icon: 'receipt'
+                        },
+                    ];
                     alertsService.alerts.subscribe(function (alert) { return _this.alerts.push(alert); });
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'mp-garden',
-                        template: "\n<h1 class='title'>The Pleimanns' - Garden Info</h1>\n\n<nav>\n    <a [routerLink]=\"['/feeds']\">Feeds</a>\n</nav>\n\n<section *ngIf=\"alerts.size > 0\">\n  <div class=\"alert\" *ngFor=\"let alert of alerts\">\n    {{alert.message}}\n  </div>    \n</section>\n\n<router-outlet></router-outlet>\n  ",
-                        directives: [router_1.ROUTER_DIRECTIVES, common_1.COMMON_DIRECTIVES],
-                        precompile: [feed_list_component_1.FeedListComponent, feed_detail_component_1.FeedDetailComponent]
+                        template: "\n<header>\n\t<md-toolbar [color]=\"'accent'\">\n\t  <md-toolbar-row>{{ title }}</md-toolbar-row>\n\t  \n\t  <md-toolbar-row>\n      <md-nav-list>\n        <button md-button *ngFor=\"let view of views\" [routerLink]=\"[ view.name ]\">\n          <md-icon md-list-icon fontIcon=\"{{ view.icon }}\"></md-icon>\n          <span>{{ view.name | uppercase }}</span>\n          <span>{{ view.description }}</span>\n        </button>\n      </md-nav-list>\n\t\t</md-toolbar-row>\n\t\t\n\t\t<span class=\"app-toolbar-filler\"></span>\n\t</md-toolbar>\n\t<div class=\"app-content\">\n    <section>\n      <div class=\"alert\" *ngFor=\"let alert of alerts\">\n        {{ alert.message }}\n      </div>    \n    </section>\n\n\t\t<router-outlet></router-outlet>\n\t</div>\n</header>\n",
+                        pipes: [common_1.COMMON_PIPES, common_1.UpperCasePipe],
+                        directives: [router_1.ROUTER_DIRECTIVES, common_1.COMMON_DIRECTIVES, icon_1.MD_ICON_DIRECTIVES, sidenav_1.MD_SIDENAV_DIRECTIVES, button_1.MD_BUTTON_DIRECTIVES],
+                        providers: [icon_1.MdIconRegistry],
+                        precompile: [feed_list_component_1.FeedListComponent, feed_detail_component_1.FeedDetailComponent] // required for @angular/router
                     }), 
                     __metadata('design:paramtypes', [alerts_service_1.AlertsService])
                 ], AppComponent);
--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,34 +1,73 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { COMMON_DIRECTIVES } from "@angular/common";
+import { COMMON_DIRECTIVES, COMMON_PIPES, UpperCasePipe } from "@angular/common";
+
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
 
 import { FeedListComponent } from './feeds/feed-list.component';
 import { FeedDetailComponent } from './feeds/feed-detail.component';
 
 import { Alert, AlertsService } from './alerts.service';
 
+interface View {
+  name: string;
+  description: string;
+  icon: string;
+}
+
 @Component({
   selector: 'mp-garden',
   template: `
-<h1 class='title'>The Pleimanns' - Garden Info</h1>
+<header>
+	<md-toolbar [color]="'accent'">
+	  <md-toolbar-row>{{ title }}</md-toolbar-row>
+	  
+	  <md-toolbar-row>
+      <md-nav-list>
+        <button md-button *ngFor="let view of views" [routerLink]="[ view.name ]">
+          <md-icon md-list-icon fontIcon="{{ view.icon }}"></md-icon>
+          <span>{{ view.name | uppercase }}</span>
+          <span>{{ view.description }}</span>
+        </button>
+      </md-nav-list>
+		</md-toolbar-row>
+		
+		<span class="app-toolbar-filler"></span>
+	</md-toolbar>
+	<div class="app-content">
+    <section>
+      <div class="alert" *ngFor="let alert of alerts">
+        {{ alert.message }}
+      </div>    
+    </section>
 
-<nav>
-    <a [routerLink]="['/feeds']">Feeds</a>
-</nav>
-
-<section *ngIf="alerts.size > 0">
-  <div class="alert" *ngFor="let alert of alerts">
-    {{alert.message}}
-  </div>    
-</section>
-
-<router-outlet></router-outlet>
-  `,
-  directives: [ ROUTER_DIRECTIVES, COMMON_DIRECTIVES ],
-  precompile: [ FeedListComponent, FeedDetailComponent ]
+		<router-outlet></router-outlet>
+	</div>
+</header>
+`,
+  pipes: [ COMMON_PIPES, UpperCasePipe ],
+  directives: [ ROUTER_DIRECTIVES, COMMON_DIRECTIVES, MD_ICON_DIRECTIVES, MD_SIDENAV_DIRECTIVES, MD_BUTTON_DIRECTIVES ],
+  providers: [ MdIconRegistry ],
+  precompile: [ FeedListComponent, FeedDetailComponent ] // required for @angular/router
 })
 export class AppComponent {
   public alerts: Array<Alert> = [];
+  public title: string = "Pleimann - Garden Dashboard";
+
+  public views: View[] = [
+    {
+      name: 'feeds',
+      description: 'Current weather in St. Louis',
+      icon: 'receipt'
+    },
+    // {
+    //   name: 'moisture',
+    //   description: 'Soil moisture level of garden beds',
+    //   icon: 'fa-tint'
+    // }
+  ];
 
   constructor(alertsService: AlertsService) {
     alertsService.alerts.subscribe((alert: Alert) => this.alerts.push(alert));
